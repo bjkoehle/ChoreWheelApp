5ff19acc89c890daab613d7b01de10906dd013af
Tik Aw <Tik Aw>
2017-03-29 13:42:33 -0700

    ignore again fuck you brendan

---
+++ App/chorewheel/RegisterScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, Image, View } from 'react-native'
+
+export default class RegisterScreen extends React.Component {
+  render(){
+    return(
+      <View>
+        <Text>Hello Cuk!</Text>
+      </View>
+    )
+  }
+}
